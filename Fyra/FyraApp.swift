5aef12fd4ce03a21fdfa8e0c42a20517c3d63a38
Yash Patel <107436541+YashPatel1604@users.noreply.github.com>
2026-01-31 18:18:57 -0800

    Initial Commit

---
+++ Fyra/FyraApp.swift
@@ -0,0 +1,17 @@
+//
+//  FyraApp.swift
+//  Fyra
+//
+//  Created by Yash Patel on 31/01/26.
+//
+
+import SwiftUI
+
+@main
+struct FyraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
